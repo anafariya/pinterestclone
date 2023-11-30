e16d1898e128fb23cd996a2205ab08c5f9b80f1a
Ana Fariya <anafariya@gmail.com>
2023-11-30 16:36:51 +0530

    learning backend

---
+++ routes/users.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post', // Assuming you have a Post model defined separately
+    }
+  ],
+  dp: {
+    type: String, // Assuming dp is a URL or file path to the display picture
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
